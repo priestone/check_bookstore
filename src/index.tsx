8c970c231dbc9915d5095b43a4f805ce7536298f
preistone <aion97@naver.com>
2025-02-24 11:47:02 +0900

    router setting

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Router from "./Router";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<React.StrictMode></React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    <Router></Router>
+  </React.StrictMode>
+);
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+const Home = () => {
+  return <Container>홈입니다.</Container>;
+};
+
+export default Home;
